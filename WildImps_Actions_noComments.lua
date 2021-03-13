871aef232e20808876b0b3b77c56ec99e79bed5f
daniel rossa <daniel.rossa@rossa-fam.local>
2021-03-13 10:56:04 +0100

    release version 2.0.3

---
+++ WildImps_Actions_noComments.lua
@@ -0,0 +1,96 @@
+aura_env.lastTyrant = GetTime();
+
+function aura_env.AddImp(allstates,petSumd,petType)
+
+    local expTime;
+    
+    if aura_env.lastTyrant > GetTime() then
+        expTime = aura_env.lastTyrant + 20;
+    else
+        expTime = GetTime() + 20;
+    end
+    
+    allstates[petSumd] = {
+        show = true,
+        progressType = "static",
+        icon = 460856,
+        tyrantActive = aura_env.lastTyrant > GetTime() and true or false,
+        total = 100,
+        value = 100,
+        name = petType,
+        duration = expTime - GetTime(),
+        expirationTime = expTime,
+    }
+    
+    return true;
+end
+
+function aura_env.GetImpValue(allstates,caster)
+
+    allstates[caster].value = allstates[caster].value - 20;
+    allstates[caster].changed = true;
+
+    if allstates[caster].value <= 0 then
+        allstates[caster].show = false;
+        allstates[caster].changed = true;
+        allstates[caster] = nil;
+        aura_env.impCount = aura_env.impCount - 1;
+    end
+    return true;
+end
+
+function aura_env.ClearImps(allstates)
+
+    for imp,state in pairs(allstates) do
+
+        state.show = false;
+        state.changed = true;
+        imp = nil;
+    end
+    return true;
+end
+
+function aura_env.TyrantSummoned(allstates,subEvent,seconds)
+
+    if subEvent == "SPELL_CAST_SUCCESS" then
+
+        local _,_,_,_,talentSelected = 4, GetTalentInfo(7, 2, 1);        
+        aura_env.lastTyrant = GetTime() + seconds;
+
+        if talentSelected then
+            aura_env.ClearImps(allstates);
+        end        
+
+    else
+
+        if aura_env.lastTyrant < GetTime() + seconds then
+            aura_env.lastTyrant = GetTime() + seconds;
+        end
+        aura_env.ExtendImpDuartion(allstates,seconds);
+
+    end
+    return true;
+end
+
+function aura_env.GetTyrantActive(allstates)
+
+    if aura_env.lastTyrant and (aura_env.lastTyrant >= GetTime()) then
+        for _,state in pairs(allstates) do
+            state.tyrantActive = true;
+        end
+    else
+        for _,state in pairs(allstates) do
+            state.tyrantActive = false;
+        end
+    end
+end
+
+function aura_env.ExtendImpDuartion(allstates,seconds)
+
+    for _,state in pairs(allstates) do
+
+        state.expirationTime = state.expirationTime + seconds;
+        state.tyrantActive = true;
+        state.changed = true;
+    end
+end
