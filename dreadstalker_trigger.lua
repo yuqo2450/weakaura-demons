fac3b0f17a198233d142fc4ca760bf479980e009
Daniel Rossa <danielrossa92@gmail.com>
2023-11-08 07:47:38 +0100

    fixed: there was an update so that the unit in CLEU is returned with unitname-realm

--- dreadstalker_trigger.lua
+++ dreadstalker_trigger.lua
@@ -1,18 +1,22 @@
 --[[ events: CLEU:SPELL_SUMMON:SPELL_CAST_SUCCESS ]]
 function(allstates,event,_,subEvent,...)
-  local caster = select(3,...);
+  local unit = select(3,...);
   local creature = select(6,...);
   local pet = select(11,...);
   local spell = select(10,...);
 
-  if subEvent == "SPELL_SUMMON" and pet == "Call Dreadstalkers" and caster == UnitName("player") then
+  if unit ~= UnitFullName("player").."-"..GetRealmName() then
+    return false;
+  end
+
+  if subEvent == "SPELL_SUMMON" and pet == "Call Dreadstalkers" then
     local _,petName = strsplit(" ",pet);
 
     aura_env.CreateStates(allstates,creature,petName);
     return true;
   end
 
-  if subEvent == "SPELL_CAST_SUCCESS" and caster == UnitName("player") and spell == 265187 then
+  if subEvent == "SPELL_CAST_SUCCESS" and spell == 265187 then
     aura_env.UpdateOnTyrant(allstates);
     return true;
   end
