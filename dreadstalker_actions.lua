7542ba32d5d078179f795a81e61f23c6b97970ac
Daniel Rossa <danielrossa92@gmail.com>
2023-11-08 07:28:40 +0100

    added: actions code to create functions

---
+++ dreadstalker_actions.lua
@@ -0,0 +1,19 @@
+function aura_env.CreateStates(allstates,creature,petName)
+  allstates[creature] = {
+    show = true,
+    changed = true,
+    progressType = "timed",
+    icon = 1378282,
+    duration = 12,
+    expirationTime = GetTime() + 12,
+    autoHide = true,
+    name = petName,
+  };
+end
+
+function aura_env.SetTime(allstates)
+  for _,state in pairs(allstates) do
+    state.expirationTime = state.expirationTime + 15;
+    state.changed = true;
+  end
+end
--- dreadstalker_trigger.lua
+++ dreadstalker_trigger.lua
@@ -1,30 +1,17 @@
 --[[ events: CLEU:SPELL_SUMMON:SPELL_CAST_SUCCESS ]]
 function(allstates,event,_,subEvent,...)
-
   local _,_,caster,_,_,creatureName,_,_,_,_,pet = ...;
   local _,_,_,_,_,_,_,_,_,spell = ...;
 
   if subEvent == "SPELL_SUMMON" and pet == "Call Dreadstalkers" and caster == UnitName("player") then
-    local _,petName = strsplit(" ",pet)
+    local _,petName = strsplit(" ",pet);
 
-    allstates[creatureName] = {
-      show = true,
-      changed = true,
-      progressType = "timed",
-      icon = 1378282,
-      duration = 12,
-      expirationTime = GetTime() + 12,
-      autoHide = true,
-      name = petName,
-    }
+    aura_env.CreateStates(allstates,creatureName,petName);
     return true;
   end
 
   if subEvent == "SPELL_CAST_SUCCESS" and caster == UnitName("player") and spell == 265187 then
-    for _,state in pairs(allstates) do
-      state.expirationTime = state.expirationTime + 15;
-      state.changed = true;
-    end
+    aura_env.SetTime(allstates);
     return true;
   end
 end
