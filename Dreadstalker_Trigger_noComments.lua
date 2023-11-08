e00bc5e23a6b0e8e1e3471dd77f002b60c274c84
Daniel Rossa <danielrossa92@gmail.com>
2023-11-08 07:15:13 +0100

    changed: refactored code for better readability

--- Dreadstalker_Trigger_noComments.lua
+++ Dreadstalker_Trigger_noComments.lua
@@ -1,35 +1,30 @@
+--[[ events: CLEU:SPELL_SUMMON:SPELL_CAST_SUCCESS ]]
 function(allstates,event,_,subEvent,...)
 
-    local _,_,caster,_,_,creatureName,_,_,_,_,pet = ...;
+  local _,_,caster,_,_,creatureName,_,_,_,_,pet = ...;
+  local _,_,_,_,_,_,_,_,_,spell = ...;
 
-    if subEvent == "SPELL_SUMMON" and pet == "Call Dreadstalkers" and caster == UnitName("player") then
+  if subEvent == "SPELL_SUMMON" and pet == "Call Dreadstalkers" and caster == UnitName("player") then
+    local _,petName = strsplit(" ",pet)
 
-        local _,petName = strsplit(" ",pet)
-        
-        allstates[creatureName] = {
-            
-            show = true,
-            changed = true,
-            progressType = "timed",
-            icon = 1378282,
-            duration = 12,
-            expirationTime = GetTime() + 12,
-            autoHide = true,
-            name = petName,
-        }
-        return true;
+    allstates[creatureName] = {
+      show = true,
+      changed = true,
+      progressType = "timed",
+      icon = 1378282,
+      duration = 12,
+      expirationTime = GetTime() + 12,
+      autoHide = true,
+      name = petName,
+    }
+    return true;
+  end
+
+  if subEvent == "SPELL_CAST_SUCCESS" and caster == UnitName("player") and spell == 265187 then
+    for _,state in pairs(allstates) do
+      state.expirationTime = state.expirationTime + 15;
+      state.changed = true;
     end
-
-    local _,_,_,_,_,_,_,_,_,spell = ...;
-
-    if subEvent == "SPELL_CAST_SUCCESS" and caster == UnitName("player") and spell == 265187 then
-
-        for _,state in pairs(allstates) do
-            
-            state.expirationTime = state.expirationTime + 15;
-            state.changed = true;
-        end
-        
-        return true;
-    end
+    return true;
+  end
 end
