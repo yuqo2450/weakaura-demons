256a4370521b7cccca6580304241d65697a50ca9
Daniel Rossa <danielrossa92@gmail.com>
2023-11-09 10:54:00 +0100

    changed: variable names for better reading

--- wildimps_trigger.lua
+++ wildimps_trigger.lua
@@ -1,6 +1,6 @@
 --[[ events: CLEU:SPELL_SUMMON:SPELL_CAST_SUCCESS:SPELL_AURA_APPLIED,FRAME_UPDATE ]]
 function(allstates,event,time,subEvent,...)
-  local guid = select(2,...);
+  local guidCaster = select(2,...);
   local spell = select(10,...);
   aura_env.GetTyrantActive(allstates);
 
@@ -9,32 +9,32 @@ function(allstates,event,time,subEvent,...)
     aura_env.SetProgressType(allstates);
   end
 
-  if subEvent == "SPELL_SUMMON" and guid == UnitGUID("player") then
-    local imp = select(6,...);
+  if subEvent == "SPELL_SUMMON" and guidCaster == UnitGUID("player") then
+    local guidSummoned = select(6,...);
     local demon = select(7,...);
 
     if demon == "Wild Imp" then
-      aura_env.AddImp(allstates,imp,demon);
+      aura_env.AddImp(allstates,guidSummoned,demon);
     elseif demon == "Demonic Tyrant" then
       aura_env.TyrantSummoned(allstates);
     else
       return false;
     end
   elseif subEvent == "SPELL_CAST_SUCCESS" then
-    if spell == 104318 and allstates[guid] ~= nil then
-        aura_env.GetImpValue(allstates,guid);
-    elseif spell == 196277 and guid == UnitGUID("player") then
+    if spell == 104318 and allstates[guidCaster] ~= nil then
+        aura_env.GetImpValue(allstates,guidCaster);
+    elseif spell == 196277 and guidCaster == UnitGUID("player") then
       aura_env.ClearImps(allstates);
-    elseif spell == 265187 and guid == UnitGUID("player") then
+    elseif spell == 265187 and guidCaster == UnitGUID("player") then
       aura_env.tyrantCast = true;
     else
       return false;
     end
   elseif subEvent == "SPELL_AURA_APPLIED" then
-    local imp = select(6,...);
+    local guidApplied = select(6,...);
     local demon = select(11,...);
-    if spell == 387458 and allstates[imp] ~= nil then
-      aura_env.SetImpType(allstates, imp, demon)
+    if spell == 387458 and allstates[guidApplied] ~= nil then
+      aura_env.SetImpType(allstates, guidApplied, demon)
     end
   end
   return true;
